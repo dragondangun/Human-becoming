6149f6f6806e9b5571f31e24bd73bbf22459abdf
dragondangun <dan.sura@mail.ru>
2023-05-01 16:51:39 +0300

    Citing

--- script.py
+++ script.py
@@ -4,6 +4,14 @@ abstractFile.close()
 
 textToAdd = textToAdd.replace('--', '—')
 
+citeCounter = 1
+point = textToAdd.find('%cite%')
+while(point != -1):
+    textToAdd = textToAdd[:point] + str(citeCounter) + textToAdd[point+6:]
+    point = textToAdd.find('%cite%')
+    citeCounter += 1
+
+
 mainFile = open("docs/index.html", "w", encoding="utf8")
 title = 'Становление человечества' # Enter title
 mainFile.write('<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset="utf-8">\n\t\t<title>Конспект '+title+'</title>\n\t\t<link rel="stylesheet" href="style.css">\n\t</head>\n\t<body>\n\t\t<div class="MyDiv">\n')
